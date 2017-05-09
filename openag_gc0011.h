c628b89c826e12436ab3f9f30d1b305a2c98f06d
Joseph Juhnke <joe@novemberkiloecho.com>
2017-05-09 12:25:53 -0500

    Update openag_gc0011.h

--- openag_gc0011.h
+++ openag_gc0011.h
@@ -1,30 +1,12 @@
 /**
  *  \file openag_gc0011.h
- *  \brief Sensor module for co2 count parts per million
+ *  \brief Carbon Dioxide Sensor for PFC1 BOM
  */
 
 #ifndef OPENAG_GC0011_H
 #define OPENAG_GC0011_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-
-// 8 MHz(ish) AVR ---------------------------------------------------------
-#if (F_CPU >= 7400000UL) && (F_CPU <= 9500000UL)
-#define COUNT 3
-// 16 MHz(ish) AVR --------------------------------------------------------
-#elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000L)
-#define COUNT 6
-#else
-#error "CPU SPEED NOT SUPPORTED"
-#endif
-
-// how many timing transitions we need to keep track of. 2 * number bits + extra
-#define MAXTIMINGS 85
-
+#include "Arduino.h"
 #include <openag_module.h>
 #include <std_msgs/Float32.h>
 #include <SoftwareSerial.h>
@@ -34,8 +16,9 @@
  */
 class Gc0011 : public Module {
   public:
-    // Public Functions
     Gc0011(int rx_pin, int tx_pin);
+ 
+    // Public Functions
     void begin();
     void update();
     bool get_air_carbon_dioxide(std_msgs::Float32 &msg);
@@ -49,6 +32,7 @@ class Gc0011 : public Module {
     // Private Variables
     int _rx_pin;
     int _tx_pin;
+ 
     float _carbon_dioxide;
     bool _send_carbon_dioxide;
     uint32_t _time_of_last_reading;
@@ -62,6 +46,9 @@ class Gc0011 : public Module {
     uint8_t _ind;
     uint32_t _last_read_time;
     bool _first_reading;
+ 
+    // Private functions
+    void readData();
 
     // Status codes
     static const uint8_t CODE_FAILED_TO_READ = 1;
